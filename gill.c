6a753fc37ca89299c925e56141efca2bf1e728aa
andreasenviggo <viggo@ruc.dk>
2020-08-10 15:40:38 +0200

    Add files via upload

---
+++ gill.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define N 10000 /* Pop storrelse */
+#define F 5000  /* anat familier */
+#define C 2 /* antal sociale contexter */
+
+int social[N][C];
+
+
+void initialize_social();
+
+
+int main() {
+	printf("tester\n");
+	/* hej Christian */
+	return 0;
+}
+ new_decol_day=(int)floor((double)random()/RAND_MAX*community_days);
+
+
